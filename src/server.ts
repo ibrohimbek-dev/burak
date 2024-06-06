712d7c635b6da7e7d3d0cecc80661d4cc17811e0
Ibrohimbek-Dev <programming0127@gmail.com>
2024-06-06 10:27:44 +0900

    feat: MongoDB is connected via mongoose

--- src/server.ts
+++ src/server.ts
@@ -8,4 +8,19 @@
 
 // =================================================================
 
+// Mongoose bu MongoDB bilan ishlash uchun hosil qilingan maxsus package hisoblanadi
 
+// Connect MongoDB via Mongoose
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL as string, {})
+  .then((data) => {
+    console.log("MongoDB connected successfully!");
+    // const PORT = process.env.PORT ?? 3003;
+  })
+  .catch((err) => {
+    console.log("An error occured whie connecting to MongoDB:", err.message);
+  });
