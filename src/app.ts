4d1498944ca3eb157454a3ebd7c70a8c88b45c74
Ibrohimbek-Dev <programming0127@gmail.com>
2024-06-06 10:45:55 +0900

    feat: express integrations and 37th lesson is finished here

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import path from "path";
+
+// Express Has Four Main Structures:
+
+// 1) ENTERANCE
+const app = express();
+
+// Middleware pattern:
+// console.log("__dirname:", __dirname);
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// 2) SESSIONS
+
+// 3) VIEWS
+app.set("view", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// 4) ROUTERS
+
+// Export app.ts in order to use it in another file
+export default app; // module.exports = app
--- src/server.ts
+++ src/server.ts
@@ -13,13 +13,18 @@
 // Connect MongoDB via Mongoose
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import app from "./app";
+
 dotenv.config();
 
 mongoose
   .connect(process.env.MONGO_URL as string, {})
   .then((data) => {
     console.log("MongoDB connected successfully!");
-    // const PORT = process.env.PORT ?? 3003;
+    const PORT = process.env.PORT ?? 3003;
+    app.listen(PORT, () => {
+      console.log(`Server is running on port: ${PORT}`);
+    });
   })
   .catch((err) => {
     console.log("An error occured whie connecting to MongoDB:", err.message);
