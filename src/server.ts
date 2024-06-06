8d2bdbb18a5e26171b7d3e5867c82ac5e6f450ca
Ibrohimbek-Dev <programming0127@gmail.com>
2024-06-06 09:32:53 +0900

    feat: 36th lesson is started here

--- src/server.ts
+++ src/server.ts
@@ -1,40 +1,10 @@
-// 2024-06-04
-// 35th lesson
-// TypeScript & Patterns
+// 2024-06-06
+// 36th lesson
+// Environmental variablar va loyihamiz uchun yangi database
 
-// -------------------------------------------------------------------------
-// Har qanday Front-end yoki Backend ma'lum bit Pattern'lar asosida quriladi
-// Asosan backend uchun bir necha xil programming language'lardan foydalanishimiz mumkin: Java, PHP, NodeJS, Python, GoLang, etc.
+// Darsimiz rejasi:
+// 1) Environmental variable'larni integratsiyasini amalga oshiramiz
+// 2) Burak loyihasi uchun yangi database'ni xosil qilamiz
 
-// Har qaday programming tillaridan foydalanishimizga qaramasdan biz doim, Architectural va Pattern'lariga murojaat qilamiz
-
-// Va bular language'lar aro bir xil tushunchalar
-// Agar siz shu Architectural va Design Patternlarni yaxshi tushunib olsangiz, bir tildan boshqa bir tilga o'tishingiz juda ham oson kechadi
-
-
-// ------------------------------------------------------------------------
-// Backend bo'yicha eng mashxur Architectural Pattern'lar:
-// 1) MVC => Model-View-Controller
-// 2) DI => Dependency Injection
-
-// Frontend bo'yicha eng mashxur architectural pattern'lar:
-// 1) MVP => Model-View-Presenter
-
-// Backend bo'yicha eng mashxur Design Pattern'lar:
-// 1) Middleware
-// 2) Decorator
-
-// -----------------------------------------------------------------------
-// Architectural Pattern => Bu backend uchun suyak hisoblanadi. Ya'ni backendimizdagi ma'lumotlar oqimini tartibga soladigan arxitekturasi hisoblanadi
-// Design Pattern => Butun bir backend tizimidagi ma'lum bir bo'laklaridagi struktularni yechishda xizmat qiladigan pattern hisoblanadi (vositachi emas).
-
-
-// Architectural Pattern'ni misol sifatida butun boshlik insonning suyagi desak, Design Pattern bu o'sha insonning qo'l suyagi hisoblanadi. Ya'ni qo'l bu ma'lum bir vazifani bajaradigon bo'lim hisoblanadi.
-// Architectural Pattern esa o'sha insonning o'zi hisoblanadi
-
-
-// 35th - dars shu yerda yakunlandi
-// 수고 하셨습니다!
-
-
+// =================================================================
 
