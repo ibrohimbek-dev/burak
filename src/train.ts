818959327c37f6cab6b7be6fb2f7a6f650254bbf
Ibrohimbek-Dev <programming0127@gmail.com>
2024-06-08 16:25:46 +0900

    TASK H2 is completed

--- src/train.ts
+++ src/train.ts
@@ -1,6 +1,24 @@
-// H2-TASK: 
+// 2024-06-08
+// MIT 14
+// 39th Lesson TASK H2
 
-// Shunday function tuzing, unga string argument pass bolsin. Function ushbu agrumentdagi digitlarni yangi stringda return qilsin
-// MASALAN: getDigits("m14i1t") return qiladi "141"
+// String argumnet pass bo'ladigan function tuzing.
+// Ushbu function argument tarkibidagi digit(son)larni topib
+// yangi stringda return qilsin
 
-// @steezyworld MITASK
+// MASALAN: getDigits("m14i1t"); return qiladi "141"
+// Yuqoridagi misolda, berilayotgan parametr tarkibida ham harf ham son mavjud.
+// Natija sifatida text tarkibidagi sonlarni aniqlab ularni string ko'rinishida qaytarmoqdamiz.
+
+// ==========================================================================
+
+console.log("METHOD ONE");
+const getDigits_1 = (text: string) => {
+  return text
+    .split("")
+    .filter((lt) => parseInt(lt))
+    .join("");
+};
+
+const result_1 = getDigits_1("m14i1t");
+console.log(`(method one) result: '${result_1}'`);
