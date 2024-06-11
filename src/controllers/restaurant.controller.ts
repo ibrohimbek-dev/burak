caac47077088ede02b220a23acef780f0029bed8
Ibrohimbek-Dev <programming0127@gmail.com>
2024-06-11 11:24:34 +0900

    feat: create restaurant memeber related router get and psot end points

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -8,6 +8,7 @@ restaurantController.goHome = (req: Request, res: Response) => {
 	try {
 		console.log("goHome");
 		res.send("Home Page!");
+		// send | json | redirect | end | render
 	} catch (err: any) {
 		console.log("Error on Home Page:", err.message);
 	}
@@ -15,7 +16,7 @@ restaurantController.goHome = (req: Request, res: Response) => {
 
 restaurantController.getLogin = (req: Request, res: Response) => {
 	try {
-		res.send("Login Page!");
+		res.send("getLogin!");
 	} catch (err: any) {
 		console.log("Error on Login Page:", err.message);
 	}
@@ -23,9 +24,25 @@ restaurantController.getLogin = (req: Request, res: Response) => {
 
 restaurantController.getSignup = (req: Request, res: Response) => {
 	try {
-		res.send("Signup Page!");
+		res.send("getSignup!");
 	} catch (err: any) {
 		console.log("Error on Signup Page:", err.message);
+	}
+};
+
+restaurantController.processLogin = (req: Request, res: Response) => {
+	try {
+		res.send("processLogin!");
+	} catch (err: any) {
+		console.log("Error on processLogin:", err.message);
+	}
+};
+
+restaurantController.processSignup = (req: Request, res: Response) => {
+	try {
+		res.send("processSignup!");
+	} catch (err: any) {
+		console.log("Error on processSignup:", err.message);
 	}
 };
 
