502b94a6861849cffc4dd00119e9223f39ae304c
Ibrohimbek-Dev <programming0127@gmail.com>
2024-06-11 21:10:14 +0900

    feat: 41th lesson is finished here and develop sign up process

--- docs.ts
+++ docs.ts
@@ -1,10 +1,10 @@
 // 2024-06-11
-// 40th lesson
-// Build Project Standards
+// 41th lesson
+// Member Service Model (admin signup)
 
 // Darsimiz rejasi:
-// 1) Loyihamizni standartlarini joriy qilamiz
-// 2) API request methodlari xaqida gaplashamiz hamda postmanni siz bilan birga ishlatamiz
+// 1) API o'zi nima va uning qanday turlari mavjud
+// 2) Adminka loyihasi uchun mo'ljallangan signup API'ni to'liq develop qilamiz
 
 // =================================================================
 
@@ -22,3 +22,30 @@
 //                      folder, file => keb-bab-case                        // fol-der-name
 //                      css => sna_ke_case                            // btn_style
 // 3) Error Handlings
+
+// ==================================================================
+
+// API ==> Aplication Programming Interface
+
+// API explaned as a restaurant:
+// Application => Customer
+// Waiter => API
+// Kitchen => Server
+
+// Customer => Waiter => Kitchen => Waiter => Customer
+
+// =================================================================
+
+// Eng ko'p ishlatiladigan API turlari:
+// 1) Traditional API (api)
+// 2) Rest API
+// 3) GraphQL API
+
+// ======================================================================================================
+// Qachon Promise ishlatamiz?
+// Agar bizning methodimiz asynchronous bo'lmasa unda biz Promise ishlatmaymiz.
+// Promise faqatgina asynchronous method'da ishlatiladi xolos.
+
+// ================================================================================================
+
+// .exec() orqalik query'lar ketma - ketligini to'xtatishimiz mumkin. Va bu kam resurs iste'mol qiladi
--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import { T } from "../libs/types/common";
 import MemberService from "../models/Member.service";
+import { MemberInput } from "../libs/types/member";
+import { MemberType } from "../libs/enums/member.enum";
 
 const restaurantController: T = {};
 
@@ -38,11 +40,19 @@ restaurantController.processLogin = (req: Request, res: Response) => {
 	}
 };
 
-restaurantController.processSignup = (req: Request, res: Response) => {
+restaurantController.processSignup = async (req: Request, res: Response) => {
 	try {
-		res.send("processSignup!");
+		console.log("processSignup!");
+
+		const newMember: MemberInput = req.body;
+		newMember.memberType = MemberType.RESTAURANT;
+
+		const memberService = new MemberService();
+		const result = await memberService.processSignup(newMember);
+
+		res.send(result);
 	} catch (err: any) {
-		console.log("Error on processSignup:", err.message);
+		res.send(err);
 	}
 };
 
