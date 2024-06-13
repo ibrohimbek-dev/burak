496a9535e28a8f1dc5cb1ffce1551c6245627d7a
Ibrohimbek-Dev <programming0127@gmail.com>
2024-06-13 12:02:25 +0900

    43th lesson finished here

--- docs.ts
+++ docs.ts
@@ -1,18 +1,24 @@
 // 2024-06-13
-// 42th lesson
-// Member Service Model (admin login & Bcrypt)
+// 43th lesson
+// web Server & Authentication
 
 // Darsimiz rejasi:
-// 1) Adminka loyihasining login API'ni develop qilamiz
-// 2) Passwordlarni hashing qilish mexanizimini ya'ni bcryptni o'rganamiz
+// 1) VPS va VPC o'zi nima?
+// 2) Serverlarning o'zaro bog'lanishi bo'yicha turlari
+// 3) Authentication nima va uni tashkillashtirish usullari
+// 4) Session authentication qanday ishlashini nazariy tahlil qilamiz
+// 5) Browserlarning xotiralarini o'rganamiz
+
+// =================================================================
+
+// Authentication usulini qurishda turli xil usullar mavjud:
+// 1) Sessions (Cookies)
+// 2) Tokens (Cookies)
+// 3) Tokens (Headers)
 
 // =================================================================
 
 
 
-// =================================================================
-
-
-
-// 42th - dars shu yerda yakunlandi
+// 43th - dars shu yerda yakunlandi
 // 수고 하셨습니다!
