71b8b28e6dd8a3060df47c82925849735d640f4a
Ibrohimbek-Dev <programming0127@gmail.com>
2024-06-21 07:50:08 +0900

    feat: TASK K is completed

--- etc/review.ts
+++
@@ -1,2 +0,0 @@
-const array: string = "donate admiral banana";
-console.log(array.split(" ").sort())
--- etc/train.ts
+++ etc/train.ts
@@ -1,60 +1,128 @@
-// 2024-06-18
+// 2024-06-21
 // MIT 14
-// TASK J:
+// TASK K:
 
-// Shunday function tuzing, u string qabul qilsin.
-// Va string ichidagi eng uzun so'zni qaytarsin.
+// Berilayotgan parametr tarkibida nechta unli harf bor
+// ekanligini aniqlovchi function tuzing
 
-// MASALAN: findLongestWord("I came from Uzbekistan!"); return "Uzbekistan!"
+// MASALAN: countVowels("string"); return 1
 
-// Yuqoridagi text tarkibida 'Uzbekistan'
-// eng uzun so'z bo'lganligi uchun 'Uzbekistan'ni qaytarmoqda
+// Yuqoridagi misolda 'string' so'zi tarkibida yagona unli harf 'i'
+// bo'lganligi uchun '1'ni qaytarmoqda
 
 // ==========================================================================
-const sentence: string = "I came from Uzbekistan!";
+// Assets:
+const vowels: string = "aeiou";
+const vowelsArr: string[] = ["a", "e", "i", "o", "u"];
 
-console.info("METHOD ONE ----------------------------------------");
-
-const findLongestWord_1 = (text: string): string => {
-	const textArr: string[] = text.split(" ");
-	const result: number[] = [];
-
-	textArr.forEach((txt) => result.push(txt.length));
-
-	return textArr[result.indexOf(Math.max(...result))];
+console.log("METHOD ONE -----------------------------------------");
+const countVowels_1 = (text: string) => {
+	return text.split("").filter((txt) => vowels.includes(txt.toLowerCase())).length;
 };
 
-const result_1 = findLongestWord_1(sentence);
-console.info("(first method) result:", result_1);
+const result_1 = countVowels_1("string");
+console.log("(method one) result:", result_1);
 
 // ==========================================================================
-console.info("METHOD TWO ----------------------------------------");
+console.log("METHOD TWO -----------------------------------------");
+const countVowels_2 = (text: string) => {
+	let found: string = "";
 
-const findLongestWord_2 = (text: string) => {
-	const textArr: string[] = text.split(" ");
+	for (let txt of text) {
+		vowels.includes(txt) ? (found += txt) : "";
+	}
 
-	return textArr.reduce((maxLength, currentValue) => {
-		return currentValue.length > maxLength.length ? currentValue : maxLength;
-	}, "");
+	return found.length;
 };
 
-const result_2 = findLongestWord_2(sentence);
-console.info("(second method) result:", result_2);
+const result_2 = countVowels_2("string");
+console.log("(method two) result:", result_2);
 
 // ==========================================================================
-console.info("METHOD THREE ----------------------------------------");
+console.log("METHOD THREE -----------------------------------------");
+const countVowels_3 = (text: string) => {
+	let found: string = "";
 
-const findLongestWord_3 = (text: string) => {
-	let checkLength: string = "";
+	for (let i = 0; i < text.length; i++) {
+		if (vowels.includes(text[i])) {
+			found += text[i];
+		}
+	}
 
-	text.split(" ").forEach((txt) => {
-		if (txt.length > checkLength.length) {
-			checkLength = txt;
+	return found.length;
+};
+
+const result_3 = countVowels_3("string");
+console.log("(method three) result:", result_3);
+
+// ==========================================================================
+console.log("METHOD FOUR -----------------------------------------");
+const countVowels_4 = (text: string) => {
+	const found: string[] = [];
+
+	text.split("").some((txt) => {
+		if (vowels.includes(txt.toLowerCase())) {
+			found.push(txt);
 		}
 	});
 
-	return checkLength;
+	return found.length;
 };
 
-const result_3 = findLongestWord_3(sentence);
-console.info("(third method) result:", result_3);
+const result_4 = countVowels_4("string");
+console.log("(method four) result:", result_4);
+
+// ==========================================================================
+console.log("METHOD FIVE -----------------------------------------");
+const countVowels_5 = (text: string) => {
+	const founded = text.split("").reduce((arr: string[], val: string) => {
+		if (vowels.includes(val.toLowerCase())) {
+			arr.push(val);
+		}
+
+		return arr;
+	}, []);
+
+	return founded.length;
+};
+
+const result_5 = countVowels_5("string");
+console.log("(method five) result:", result_5);
+
+// ==========================================================================
+console.log("METHOD SIX -----------------------------------------");
+const countVowels_6 = (text: string) => {
+	return text.split("").filter((txt) => {
+		if (vowels.indexOf(txt.toLowerCase()) > -1) {
+			return txt;
+		}
+	}).length;
+};
+
+const result_6 = countVowels_6("string");
+console.log("(method six) result:", result_6);
+
+// ==========================================================================
+console.log("METHOD SEVEN -----------------------------------------");
+const countVowels_7 = (text: string) => {
+	// const found: string[] | null = text.toLowerCase().match(/[aeiuo]/g)
+	const found: string[] | null = text.match(/[aeiuo]/gi);
+
+	return found?.length;
+};
+
+const result_7 = countVowels_7("string");
+console.log("(method seven) result:", result_7);
+
+// ==========================================================================
+console.log("METHOD EIGHT -----------------------------------------");
+const countVowels_8 = (text: string) => {
+	return text.split("").filter((txt) => {
+		if (/[aeiuo]/gi.test(txt)) {
+			return txt;
+		}
+	}).length;
+};
+
+const result_8 = countVowels_8("striIng");
+console.log("(method eight) result:", result_8);
