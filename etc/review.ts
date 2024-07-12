ce15a70c0bcbf70f9460de1d3b70ddfea26b6672
Ibrohimbek <programming0127@gmail.com>
2024-07-12 12:21:13 +0900

    feat: TASK T is completed

--- etc/review.ts
+++ etc/review.ts
@@ -1,10 +1,1 @@
-function calculate(expression: string): number {
-	const numbers: string[] = expression.split("+");
-	const numericValues: number[] = numbers.map((num) =>
-		parseInt(num.trim(), 10)
-	);
-	const sum: number = numericValues.reduce((acc, curr) => acc + curr, 0);
-	return sum;
-}
-
-console.log(calculate("8+2"));
+console.log("hello")
