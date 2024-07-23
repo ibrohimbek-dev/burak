e841c0e27b084196d954089ec1269fa5618c5da9
Ibrohimbek <programming0127@gmail.com>
2024-07-23 09:25:06 +0900

    feat: TASK Y is completed

--- etc/train.ts
+++ etc/train.ts
@@ -1,84 +1,78 @@
 // ----------------------------------------
-// 2024-07-20
+// 2024-07-23
 // MIT 14
 
-// TASK X
+// TASK Y
 
-// Shunday function yozing, uni object va string parametrlari bo'lsin.
-// Bu function, birinchi object parametri tarkibida, kalit sifatida ikkinchi string parametri
-// necha marotaba takrorlanganlini sanab qaytarsin.
+// Shunday function yozing, uni 2'ta array parametri bo'lsin.
+// Bu function ikkala arrayda ham ishtirok etgan bir xil
+// qiymatlarni yagona arrayga joylab qaytarsin.
 
-// Eslatma => Nested object'lar ham sanalsin
+//  MASALAN: findIntersection([1,2,3], [3,2,0]) return [2,3]
 
-// MASALAN: countOccurrences({model: 'Bugatti', steer: {model: 'HANKOOK', size: 30}}, 'model') return 2
-
-// Yuqoridagi misolda, birinchi argument object, ikkinchi argument 'model'.
-// Funktsiya, shu ikkinchi argument 'model', birinchi argument object
-// tarkibida kalit sifatida 2 marotaba takrorlanganligi uchun 2 soni return qilmoqda
+// Yuqoridagi misolda, argument sifatida berilayotgan array'larda
+// o'xshash sonlar mavjud. Function'ning vazifasi esa ana shu
+// ikkala array'da ishtirok etgan o'xshash sonlarni yagona arrayga
+// joylab return qilmoqda.
 
 // =====================================================
 // Assets:
+const arrOne: number[] = [3, 2, 0, 9, 101, 98, 97, 96];
+const arrTwo: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 101];
 
-const myObject: Record<string, any> = {
-	model: "Bugatti",
-	steer: {
-		model: "HANKOOK",
-		size: 30,
-	},
-};
-
-const findKey: string = "model";
-
-// ======================================================
 // Solutions:
 
 // METHOD ONE:
-console.log("------------------------------------");
-
-const countOccurrences_1 = (obj1: Record<string, any>, thisKey: string) => {
-	let count: number = 0;
-
-	const findKey = (obj2: Record<string, any>): void => {
-		if (obj2 !== null && typeof obj2 === "object") {
-			Object.keys(obj2).reduce((acc, key) => {
-				if (key === thisKey || obj2[key] === thisKey) {
-					count++;
-				}
-
-				findKey(obj2[key]);
-				return acc;
-			}, {});
-		}
-	};
-
-	findKey(obj1);
-
-	return count;
+console.log("----------------------------------------------------------");
+const findIntersection_1 = (arr1: number[], arr2: number[]) => {
+	return arr1.filter((num) => {
+		return arr2.includes(num);
+	});
 };
 
-console.log("method one =>", countOccurrences_1(myObject, findKey));
+console.log("method one =>", findIntersection_1(arrOne, arrTwo));
 
 // METHOD TWO:
-console.log("------------------------------------");
+console.log("----------------------------------------------------------");
+const findIntersection_2 = (arr1: number[], arr2: number[]) => {
+	return arr1.reduce((arr, num) => {
+		if (arr2.includes(num)) {
+			arr.push(num);
+		}
 
-const countOccurrences_2 = (obj: Record<string, any>, thisKey: string) => {
-	let count: number = 0;
-	const stack = [obj];
+		return arr;
+	}, [] as number[]);
+};
 
-	for (let i = 0; i <= stack.length; i++) {
-    const popThisObj = stack.pop();      
+console.log("method two =>", findIntersection_2(arrOne, arrTwo));
 
-		for (let key in popThisObj) {
-			if (key === thisKey) {
-				count++;
-			}
-			if (typeof popThisObj[key] === "object" && popThisObj[key] !== null) {
-				stack.push(popThisObj[key]);
-			}
+// METHOD THREE:
+console.log("----------------------------------------------------------");
+const findIntersection_3 = (arr1: number[], arr2: number[]) => {
+	const concatted = arr1.concat(arr2);
+
+	const founded = concatted.filter((num, index) => {
+		return concatted.indexOf(num) !== index;
+	});
+
+	return [...new Set(founded)];
+};
+
+console.log("method three =>", findIntersection_3(arrOne, arrTwo));
+
+// METHOD FOUR:
+console.log("----------------------------------------------------------");
+const findIntersection_4 = (arr1: number[], arr2: number[]) => {
+	const concatted: number[] = arr1.concat(arr2);
+	const result: number[] = [];
+
+	for (let i = 0; i < concatted.length; i++) {
+		if (arr1.includes(arr2[i])) {
+			result.push(arr2[i]);
 		}
 	}
 
-	return count;
+	return result;
 };
 
-console.log("method two =>", countOccurrences_2(myObject, findKey));
+console.log("method four =>", findIntersection_4(arrOne, arrTwo));
