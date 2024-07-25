fd5bd3857830b1233cf2f76faa0c07edd0aa429a
Ibrohimbek <programming0127@gmail.com>
2024-07-26 07:16:31 +0900

    feat: TASK Z is completed

--- etc/train.ts
+++ etc/train.ts
@@ -1,78 +1,107 @@
 // ----------------------------------------
-// 2024-07-23
+// 2024-07-26
 // MIT 14
 
-// TASK Y
+// TASK Z
 
-// Shunday function yozing, uni 2'ta array parametri bo'lsin.
-// Bu function ikkala arrayda ham ishtirok etgan bir xil
-// qiymatlarni yagona arrayga joylab qaytarsin.
+// Shunday function yozing. Bu function sonlardan iborat array
+// qabul qilsin. Function'ning vazifasi array tarkibidagi juft
+// sonlarni topib ularni yig'disini qaytarsin.
 
-//  MASALAN: findIntersection([1,2,3], [3,2,0]) return [2,3]
+// MASALAN:
+// sumEvens([1, 2, 3]); return 2;
+// sumEvens([1, 2, 3, 2]); return 4;
 
-// Yuqoridagi misolda, argument sifatida berilayotgan array'larda
-// o'xshash sonlar mavjud. Function'ning vazifasi esa ana shu
-// ikkala array'da ishtirok etgan o'xshash sonlarni yagona arrayga
-// joylab return qilmoqda.
+// Yuqoridagi misolda, bizning funktsiya
+// berilayotgan array tarkibidagi sonlar ichidan faqatgina juft bo'lgan
+// sonlarni topib, ularni hisoblab yig'indisini qaytarmoqda.
 
 // =====================================================
 // Assets:
-const arrOne: number[] = [3, 2, 0, 9, 101, 98, 97, 96];
-const arrTwo: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 101];
+const myArrNums: number[] = [1, 2, 3, 2, 1];
 
 // Solutions:
 
 // METHOD ONE:
-console.log("----------------------------------------------------------");
-const findIntersection_1 = (arr1: number[], arr2: number[]) => {
-	return arr1.filter((num) => {
-		return arr2.includes(num);
-	});
+console.log("------------------------------------");
+const sumEvens_1 = (myArr: number[]) => {
+	return myArr.reduce((arr, num) => {
+		if (num % 2 === 0) {
+			return arr + num;
+		}
+		return arr;
+	}, 0);
 };
 
-console.log("method one =>", findIntersection_1(arrOne, arrTwo));
+console.log("method one =>", sumEvens_1(myArrNums));
 
 // METHOD TWO:
-console.log("----------------------------------------------------------");
-const findIntersection_2 = (arr1: number[], arr2: number[]) => {
-	return arr1.reduce((arr, num) => {
-		if (arr2.includes(num)) {
-			arr.push(num);
-		}
+console.log("------------------------------------");
+const sumEvens_2 = (myArr: number[]) => {
+	let sum: number = 0;
 
-		return arr;
-	}, [] as number[]);
-};
-
-console.log("method two =>", findIntersection_2(arrOne, arrTwo));
-
-// METHOD THREE:
-console.log("----------------------------------------------------------");
-const findIntersection_3 = (arr1: number[], arr2: number[]) => {
-	const concatted = arr1.concat(arr2);
-
-	const founded = concatted.filter((num, index) => {
-		return concatted.indexOf(num) !== index;
-	});
-
-	return [...new Set(founded)];
-};
-
-console.log("method three =>", findIntersection_3(arrOne, arrTwo));
-
-// METHOD FOUR:
-console.log("----------------------------------------------------------");
-const findIntersection_4 = (arr1: number[], arr2: number[]) => {
-	const concatted: number[] = arr1.concat(arr2);
-	const result: number[] = [];
-
-	for (let i = 0; i < concatted.length; i++) {
-		if (arr1.includes(arr2[i])) {
-			result.push(arr2[i]);
+	for (let i = 0; i < myArr.length; i++) {
+		if (myArr[i] % 2 === 0) {
+			sum += myArr[i];
 		}
 	}
 
-	return result;
+	return sum;
 };
 
-console.log("method four =>", findIntersection_4(arrOne, arrTwo));
+console.log("method two =>", sumEvens_2(myArrNums));
+
+// METHOD THREE:
+console.log("------------------------------------");
+const sumEvens_3 = (myArr: number[]) => {
+	return myArr.reduce(
+		(acum, curr) => {
+			if (curr % 2 === 0) acum["evens"] = acum["evens"] + curr;
+			else acum["odds"] = acum["odds"] + curr;
+			return acum;
+		},
+		{ evens: 0, odds: 0 }
+	);
+};
+
+console.log("method three =>", sumEvens_3(myArrNums));
+
+// METHOD FOUR:
+console.log("------------------------------------");
+const sumEvens_4 = (myArr: number[]) => {
+	const evens: number[] = myArr.filter((num) => num % 2 == 0);
+
+	return evens.reduce((total, num) => total + num, 0);
+};
+
+console.log("method four =>", sumEvens_4(myArrNums));
+
+// METHOD FIVE:
+console.log("------------------------------------");
+const sumEvens_5 = (myArr: number[]) => {
+	const evens: number[] = myArr.filter((num) => num % 2 == 0);
+
+	return eval([...evens].join("+"));
+};
+
+console.log("method five =>", sumEvens_5(myArrNums));
+
+// METHOD SIX:
+console.log("------------------------------------");
+const sumEvens_6 = (myArr: number[]) => {
+	const evens: number[] = myArr.filter((num) => num % 2 == 0);
+
+	return eval(evens.toString().replace(/,/g, "+"));
+};
+
+console.log("method six =>", sumEvens_6(myArrNums));
+
+// METHOD SEVEN:
+console.log("------------------------------------");
+const sumEvens_7 = (myArr: number[]) => {
+	return myArr
+		.filter((num) => num % 2 === 0)
+		.reduce((total, son) => total + son, 0);
+};
+
+console.log("method seven =>", sumEvens_7(myArrNums));
